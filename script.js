e5c844f48da494075cd3649ef1c923f9f02418d8
Ene_Dimowo <78217379+Ene-420@users.noreply.github.com>
2023-10-20 22:00:02 +0100

    fully functiional application

--- script.js
+++ script.js
@@ -1,256 +1,300 @@
-
-const gameBoard = function(){
-
-    let gameState = [2,2,2,2,2,2,2,2,2];
+const gameBoard= function(){
+    
+    let gameState = [ 2,2,2,2,2,2,2,2,2];
+    
     const winningState =[[0,1,2], [3,4,5], [6,7,8], 
                          [0,3,6], [1,4,7], [2,5,8],
                             [0,4,8], [2,4,6]]
-   
-    
+
+    //DOM elements
+    const gridBox = document.querySelector('.grid-box');
+    const gridBody = document.querySelector('.body');
+
+
+
     //render();
 
+
     
-    const item = document.querySelectorAll('.grid-item');
-    
-    
-    function play(image){
-        item.forEach((gridItem) => {
-            gridItem.addEventListener('click', enterOption(image))
-        });
-    }
-    // item.forEach((gridItem) => {
-    //     gridItem.addEventListener('click', ()=>
-    //     {
-    //         console.log(gridItem.childElementCount);
-    //     })
-    // });
 
+    function render(){      
+        
 
-     function startGame(){
-        render();
-     }
-    function render(){
-        const body = document.querySelector('.body');
-        const fillGrid = document.querySelector('.grid-box');
-
-        let countIncrement = 0;
-            for(let i=0; i< 3; i++){
-                
-                const gridContainer = document.createElement('div');
-                gridContainer.classList.add('grid-container');
-
-                for(let x= 0; x<3; x++){
-                    const gridItem = document.createElement('div');
-                    gridItem.classList.add('grid-item');
-                    gridItem.dataset.key = i+x+countIncrement;
-                    // gridItem.addEventListener('click',enterOption(gridItem));
-                    gridContainer.appendChild(gridItem);
-                }
-                countIncrement +=2;
-                fillGrid.appendChild(gridContainer);    
+        gameState.forEach((item, index) =>{
+            const gridItem = document.createElement('button');
+            gridItem.classList.add('grid-item');
+            gridItem.dataset.key = index;
+            if(gameState[gridItem.dataset.key] ==='x'){
+                const playerChoice = gridItemContent(gameState[gridItem.dataset.key]);
+                gridItem.appendChild(playerChoice);
             }
-
-            body.style.display= 'flex';
+    
+            else if(gameState[gridItem.dataset.key] ==='o'){
+                const playerChoice = gridItemContent(gameState[gridItem.dataset.key]);
+                gridItem.appendChild(playerChoice);
+            }
+            gridBox.appendChild(gridItem);
+    
+        })
+        gridBody.style.display= 'flex';
     }
 
-
-    function enterOption(event, image){
-        if(event.target.childElementCount>0){
-            console.log(event.target.childElementCount);
-            console.log('Cell already filled');
-        }
-        else{
-            console.log(event.target.childElementCount);
-            const playerChoice = document.createElement('img');
-            playerChoice.setAttribute('src', `media/${image}.png`);
-            playerChoice.classList.add('game-option');
-            event.target.appendChild(playerChoice);
-
-            event.target.dataset.source= image;
-            gameState[parseInt(event.target.dataset.key)] = event.target.dataset;
+    function gridItemContent(value){
+        switch(value){
+            case 'x':
+                const optionX =createCellOption(value);
+                return optionX
+                break;
             
-            checkPosition();
-        } 
+            case 'o':
+                const optionO =createCellOption(value);
+                return optionO;
+                break;
+
+            default:
+                break;    
+        }
+
     }
 
-    function checkPosition(){
-        if(gameState){
-            console.log(gameState);
-            winningState.forEach((state) => {
-                if(gameState[state[0]] === gameState[state[1]] && gameState[state[1]] === gameState[state[2]] && gameState[state[0]] !== 2){
-                    console.log('We have a winner');
-                    disableScreen();
-                }
-                else{
-                    console.log(state);
+    function updateScreen(){
+        const cell = document.querySelectorAll('.grid-item');
+
+        gameState.forEach((state,index) => {
+            cell.forEach(cellName => {
+                if(index === parseInt(cellName.dataset.key)){
+                    if(cellName.childElementCount === 0){
+                        if(state ==='x'){
+                            const playerChoice = gridItemContent(gameState[index]);
+                            cellName.appendChild(playerChoice);
+                        }
+                
+                        else if(state ==='o'){
+                            const playerChoice = gridItemContent(gameState[index]);
+                            cellName.appendChild(playerChoice);
+                        }
+                    }
+
                 }
             })
-        }
-    }
-    
-    function computerPLay(image){
-
+        })
     }
 
-    function enableScreen(){
-        item.disabled = false;
+    function createCellOption(value){
+        const content = document.createElement('img');
+        content.setAttribute('src', `media/${value}.png`);
+        content.classList.add('game-option');
+
+        return content;
+    };
+
+
+
+    const getGridCellNo=()=> {
+        gridBox.addEventListener('click', (event) => {
+            console.log(event.target.dataset.key);
+            return event.target.dataset.key;            
+        });
+
+        //return event.target.dataset.key;
+          
     }
 
-    function disableScreen(){
-        item.disabled = true;
-    }
-return{
-    startGame, play, enableScreen
-};
-    
-}()
+    const getGridCellState= ()=>{
+        let cellContentCount;
+        const cell = document.querySelectorAll('.grid-item');
 
-const player = function(){
+        // cell.forEach(item => item.addEventListener('click', (event)=>{
+        //     console.log(event.target)
+        //     return event.target.childElementCount;
+        // } ));
+        gridBox.addEventListener('click', (event)=>{
+            console.log(event.target.dataset.key)
+            return event.target.childElementCount;
+        });
+        
+    }
+
+
+    function getGameState(){
+        return gameState;
+    }
+
+    function setGameState(position,value){
+        gameState[position]= value;
+    }
+
+    return {getGridCellNo, getGridCellState ,render, getGameState, setGameState, updateScreen}
+}
+
+
+function createPlayer(playerName, playerChoice, playerStatus) {
+
+    const getName = ()=> playerName;
+    const getPlayerChoice  =() => playerChoice;
+    const getStatus =() => playerStatus;
+
+
+    return{playerName, playerChoice, playerStatus, getName, getPlayerChoice, getStatus};
+} 
+
+const playerController = function(){
     const player1 = document.querySelector('#player1');
-    player2_label = document.querySelector('.header-items label');
+    const player2_label = document.querySelector('.header-items label');
     const player2 = document.querySelector('#player2');
     const player2_input = document.querySelector('#player2_input');
     const checkedValue = document.querySelector("input[type='radio'][name='item']:checked")
 
-    
-    function getPlayer1(){
-        if(player1.value){
-            return player1.value;
-        }
-        else return null;
-        //(player1.value.length > 0) ? return player1.value : return null;
-    }
+    let players= [];
+    let playerId;
+    const grid = gameBoard();
 
-    function getCharacter(){
-        if(checkedValue.value){
-            return checkedValue.value;
-        }
+    player2.addEventListener('change', ()=> {
+        changePlayer2Layout(player2.value);
+    });
 
-        else return null;
-    }
-    function getPlayer2(){
-        switch (player2.value){
+    function changePlayer2Layout(value){
+        switch(value){
             case 'computer':
-                return player2.value;
+                playerId = value;
+                player2_label.style.display ='block';
+                player2_input.style.display = 'none';
                 break;
             
             case 'human':
                 player2_label.style.display ='none';
                 player2_input.style.display = 'block';
-
-                if(player2_input.value){
-                    return player2_input.value;
-                }
-                else return null;
+                playerId = value;
+                
                 break;
             
             case 'neither':
-                return 'computer';
+                console.log('doing nothing')
+                playerId=  'computer';
                 break;
+
         }
+    } 
+    function createPlayer1(){
+        if(player1.value && checkedValue){
+            const firstPlayer = createPlayer(player1.value, checkedValue.value, 'Player 1');
+            players.push(firstPlayer);
+        }
+    }  
+    
+    function createPlayer2(){
+        const player2Value = checkedValue.value ==='x' ?  'o' : 'x';
+
+        if(playerId === 'human'){
+            const secondPlayer = createPlayer(player2_input.value, player2Value , 'Player 2');
+            players.push(secondPlayer);
+        }
+
+        else if(playerId === 'computer'){
+            const secondPlayer = createPlayer(playerId, player2Value, 'Player 2');
+            players.push(secondPlayer);
+        
+        }
+        
     }
-
-    return {getPlayer1, getPlayer2, getCharacter};
-}
-
-
-const playerController =function(){
-
-    const playerDetails = player();
-
-    const players = [{
-        name: playerDetails.getPlayer1(),
-        status: 'player1'
-    },
-    {
-        name: playerDetails.getPlayer2(),
-        status: 'player2'
+    let currentPlayer;
+    const setCurrentPlayer =() => {
+        currentPlayer = players[0];
     }
-]
-    let currentPlayer= players[0];
-
-    function switchPlayer(){
-    switch(currentPlayer['status']){
-        case 'player1':
+    
+    function switchPlayers(){
+        if(currentPlayer === players[0]){
             currentPlayer = players[1];
-            break;
-        
-        case 'player2':
+            return currentPlayer;
+        }
+
+        else{
             currentPlayer = players[0];
-            break;
+            return currentPlayer;
         }
     }
 
-    const getCurrentPlayer = () => currentPlayer;
-    
-    return{getCurrentPlayer, switchPlayer};
-    
+    const getCurrentPlayer =()=> currentPlayer;
+
+    const getPlayers= ()=> players;
+
+    function playRound(activePlayer, key){
+
+        //const activePlayer = getCurrentPlayer();
+        console.log(grid.getGameState())
+        console.log(activePlayer);
+
+        if(grid.getGameState().includes(2)){
+            if(activePlayer.getName() !== 'computer'){
+                let cellCheck = grid.getGridCellState();
+                console.log(`cell check : ${cellCheck}`)
+                if(grid.getGameState()[key] === 2){
+                    grid.setGameState(key, activePlayer.getPlayerChoice());
+                    grid.updateScreen()
+                    switchPlayers();
+                    
+                    if(getCurrentPlayer().getName() === 'computer'){
+                        setTimeout(3000, computerPlay(getCurrentPlayer()));
+                    }
+                }
+            }
+
+            // else {
+            //     setTimeout(3000, computerPlay(getCurrentPlayer()));
+            // }
+        }
 
 
 }
-//const fillGrid = document.querySelector('.grid-box');
-let gameState = [2,2,2,2,2,2,2,2,2];
-const winningState =[[0,1,2], [3,4,5], [6,7,8], 
-                     [0,3,6], [1,4,7], [2,5,8],
-                        [0,4,8], [2,4,6]]
 
-// let countIncrement = 0;
-// for(let i=0; i< 3; i++){
-    
-//     const gridContainer = document.createElement('div');
-//     gridContainer.classList.add('grid-container');
+    function computerPlay(activePlayer){
+        let randomSpace = Math.round(Math.random() *9);
+        let cellState = grid.getGameState()[randomSpace];
+            if(cellState === 2){
+                grid.setGameState(randomSpace, activePlayer.getPlayerChoice());
+                grid.updateScreen();
+                switchPlayers();
+            }
+            else{
+                computerPlay(activePlayer);
+        }
 
-//     for(let x= 0; x<3; x++){
-//         const gridItem = document.createElement('div');
-//         gridItem.classList.add('grid-item');
-//         gridItem.dataset.key = i+x+countIncrement;
-//         gridContainer.appendChild(gridItem);
-//     }
-//     countIncrement +=2;
-//     fillGrid.appendChild(gridContainer);    
-// }
+    }
+    return{setCurrentPlayer, getCurrentPlayer, getPlayers, switchPlayers, createPlayer1, createPlayer2, playRound, computerPlay}
 
-
-const gridContent = document.querySelectorAll('.grid-item');
-
-// gridContent.forEach((gridBox) =>{
-//     gridBox.addEventListener('click', ()=>{
-
-//         if(gridBox.childElementCount>0){
-//             console.log(gridBox.childElementCount);
-//             console.log('Cell already filled');
-//         }
-//         else{
-//             const playerChoice = document.createElement('img');
-//             playerChoice.setAttribute('src', 'media/o.png');
-//             playerChoice.classList.add('game-option');
-//             gridBox.appendChild(playerChoice);
-
-//             gridBox.dataset.source= 'O';
-//             gameState[parseInt(gridBox.dataset.key)] = '0';
-//             console.log(gridBox.dataset.key);
-//             checkPosition();
-//         }
-        
-//     })
-    
-// })
-const gridBox = document.querySelector('.grid-box');
-
-gridBox.addEventListener('click', (event)=>{
-    console.log(event.target);
-}
-)
-
-// const player= function(name, x){
-
-// }
-
-
-function checkWinner(){
-    
 }
 
 const screenController = function(){
+    const startBtn = document.querySelector('.start-btn');
+    const headerItem = document.querySelector('.header-content');
+    const cellSpace = document.querySelector('.grid-box');
+
+    const playerCall = playerController();
+    const gridBoard = gameBoard();
+    startBtn.addEventListener('click', ()=>
+    {   
+        headerItem.style.display = 'none';
+        playerCall.createPlayer1();
+        playerCall.createPlayer2();
+        playerCall.setCurrentPlayer();
+        gridBoard.render();
+        
+
+    })
+    
+    function clickHandler(e){
+        let cellKey = parseInt(e.target.dataset.key);
+
+        
+        if(cellKey >=0){
+            playerCall.playRound(playerCall.getCurrentPlayer(), cellKey);
+        }
+        
+    }
+    cellSpace.addEventListener('click', clickHandler)
+
+    //cellSpace.forEach(cell => cell.addEventListener('click', clickHandler));
+
+    
 
 }()
